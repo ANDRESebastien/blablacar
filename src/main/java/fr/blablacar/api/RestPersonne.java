5d49cbf5e7d29932a2feed76815614f0911ef370
ANDRESebastien <andreseba_3@hotmail.com>
2018-01-29 23:18:20 +0100

    dao en cours

--- src/main/java/fr/blablacar/api/RestPersonne.java
+++ src/main/java/fr/blablacar/api/RestPersonne.java
@@ -1,8 +1,9 @@
 package fr.blablacar.api;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,21 +11,28 @@ import fr.blablacar.table.Personne;
 
 @RestController("/personne")
 public class RestPersonne {
-	
-	
-	
-	@GetMapping("/")
+
+	@GetMapping
 	public Personne lister(@RequestParam(value = "id") String idPersonne) {
-		
-		return personne;
-	}
-	
-	@PutMapping
-	public Personne modifier(@RequestParam(value = "nom", defaultValue = "defaultValue") String nom) {
 		Personne personne = new Personne();
-		
-		
 		return personne;
 	}
 
+	@PutMapping
+	public Personne modifier(@RequestParam(value = "nom", defaultValue = "defaultValue") String nom) {
+		Personne personne = new Personne();
+		return personne;
+	}
+
+	@PostMapping
+	public Personne ajouter(@RequestParam(value = "nom", defaultValue = "defaultValue") String nom) {
+		Personne personne = new Personne();
+		return personne;
+	}
+	
+	@DeleteMapping
+	public Personne supprimer(@RequestParam(value = "nom", defaultValue = "defaultValue") String nom) {
+		Personne personne = new Personne();
+		return personne;
+	}
 }
